1fc6cc2054fed9adc3877fb85c4f96d1ccd8ae0c
Purva-sonawane <144521248+Purva-sonawane@users.noreply.github.com>
2023-09-13 16:59:55 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,44 @@
+import turtle
+pen = turtle.Turtle()
+  
+def curve():
+    for i in range(200):
+  
+        pen.right(1)
+        pen.forward(1)
+def heart():
+
+    pen.fillcolor('red')
+  
+    pen.begin_fill()
+  
+    pen.left(140)
+    pen.forward(113)
+  
+    curve()
+    pen.left(120)
+
+    curve()
+
+    pen.forward(112)
+
+    pen.end_fill()
+  
+def txt():
+    pen.up()
+  
+    pen.setpos(-68, 95)
+  
+    pen.down()
+  
+    pen.color('black')
+  
+    pen.write("LOVE PURVA", font=(
+      "Verdana", 16, "italic"))
+  
+heart()
+  
+txt()
+  
+pen.ht()
+
